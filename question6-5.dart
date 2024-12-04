bc860c2cfaae587ce9ceb9f78d83a1d582baf42e
Sarawut65314812 <sarawutp65@nu.ac.th>
2024-12-04 15:02:17 +0700

    question4-6-8

---
+++ question6-5.dart
@@ -0,0 +1,24 @@
+class Camera {
+  int? id;
+  String? brand;
+  String? color;
+  double? price;
+
+  Camera(this.id, this.brand, this.color, this.price);
+@override
+  String toString() {
+     return 'id: ${this.id} name: ${this.brand} color: ${this.color} price: ${this.price}';
+    
+  }
+}
+
+void main() {
+  Camera camera1 = Camera(1, 'Nikon', 'ํYellow', 5000.0);
+  Camera camera2 = Camera(2, 'Canon', 'White', 6000.0);
+  Camera camera3 = Camera(3, 'Sony', 'Pink', 7000.0);
+
+  print(camera1.toString());        
+  print(camera2.toString());
+  print(camera3.toString());
+
+}
---
+++ question8-7.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:io';
+
+void main() async {
+  // Prompt the user for the first integer
+  print("Enter the first integer:");
+  int? num1 = int.parse(stdin.readLineSync()!);
+
+  // Prompt the user for the second integer
+  print("Enter the second integer:");
+  int? num2 = int.parse(stdin.readLineSync()!);
+
+  // Inform the user of the wait
+  print("Calculating the sum... Please wait for 3 seconds.");
+
+  // Wait for 3 seconds
+  await Future.delayed(Duration(seconds: 3));
+
+  // Calculate and print the sum
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: $sum");
+}
